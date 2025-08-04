b784050fa4ddcdb4ecdc8d6ff45d2e059cb34dca
Wallace <100392309+Darth-Degen@users.noreply.github.com>
2025-08-04 08:14:30 -0700

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import { LandingView } from "@components";
+import { NextPage } from "next";
+import dynamic from "next/dynamic";
+import { useState } from "react";
+
+//prevent lazy load and hydration error
+const PageLayout = dynamic(
+  () => import("../src/components/templates/PageLayout"),
+  { ssr: false }
+);
+
+const Home: NextPage = () => {
+  const [assets, setAssets] = useState<boolean[]>([false]);
+
+  return (
+    <PageLayout footer={false} fixed={true} assets={assets}>
+      <LandingView setAssets={setAssets} />
+    </PageLayout>
+  );
+};
+export default Home;
---
+++ src/components/templates/PageLayout.tsx
@@ -0,0 +1,76 @@
+import { FC, ReactNode, useState } from "react";
+import {
+  PageHead,
+  Header,
+  Footer,
+  SplashScreen,
+  ImageModal,
+} from "@components";
+import { enterAnimation } from "@constants";
+import { AnimatePresence, motion } from "framer-motion";
+import { useViewStore } from "src/stores";
+
+interface Props {
+  children: ReactNode;
+  footer?: boolean;
+  fixed?: boolean; //prevents scroll
+  absolute?: boolean; //allows scroll
+  headerType?: string;
+  assets?: boolean[];
+}
+
+const PageLayout: FC<Props> = (props: Props) => {
+  const {
+    footer = true,
+    fixed = false,
+    absolute = false,
+    headerType = "absolute",
+    children,
+    assets = [],
+  } = props;
+
+  const { showModal, setShowModal } = useViewStore();
+
+  return (
+    <div
+      className={`flex flex-col min-h-[100svh] h-full justify-between overflow-none ${
+        fixed ? "fixed inset-0" : absolute ? "absolute inset-0" : "relative"
+      }`}
+    >
+      <PageHead
+        title="Name"
+        description="Description"
+        url="https://addurl.xyz" // no backslash at the end
+        twitter="twitterhandle"
+      />
+      {/* header */}
+      <Header type={headerType} />
+
+      {/* body */}
+      <motion.main
+        className={`flex flex-col h-full w-full overflow-x-clip`}
+        {...enterAnimation}
+      >
+        {children}
+      </motion.main>
+
+      {/* footer */}
+      {footer && <Footer />}
+
+      {/* load screen */}
+      {assets && <SplashScreen assets={assets} />}
+
+      {/* modals */}
+      <AnimatePresence mode="wait">
+        {showModal && (
+          <ImageModal
+            key="image-modal"
+            show={showModal}
+            close={() => setShowModal(false)}
+          />
+        )}
+      </AnimatePresence>
+    </div>
+  );
+};
+export default PageLayout;
